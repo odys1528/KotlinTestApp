c0636711d1146544029768dd73951d605b1211e5
odys1528 <jolanta.zakrzewska@orange.pl>
2017-11-06 17:18:59 +0100

    no toasts, buttonUpdate function

--- app/src/main/java/com/odys/kotlintestapp/GameActivity.kt
+++ app/src/main/java/com/odys/kotlintestapp/GameActivity.kt
@@ -3,7 +3,6 @@ package com.odys.kotlintestapp
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_game.*
 
 class GameActivity : AppCompatActivity() {
@@ -13,7 +12,6 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
 
         quit_button.setOnClickListener {
-            Toast.makeText(this, "quit. back to MainActivity", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
@@ -36,9 +34,77 @@ class GameActivity : AppCompatActivity() {
         var location = locations[loc] ?: Location(0,
                     "Sorry, something went wrong, so the game will terminate")
 
+        buttonUpdate(location)
+
+        if (location.locationID == 0) {
+            disableAllButtons()
+        }
+
+        up_button.setOnClickListener {
+            loc = location.exits["U"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        down_button.setOnClickListener {
+            loc = location.exits["D"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        north_button.setOnClickListener {
+            loc = location.exits["N"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        south_button.setOnClickListener {
+            loc = location.exits["S"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        east_button.setOnClickListener {
+            loc = location.exits["E"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        west_button.setOnClickListener {
+            loc = location.exits["W"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        ne_button.setOnClickListener {
+            loc = location.exits["NE"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        nw_button.setOnClickListener {
+            loc = location.exits["NW"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        se_button.setOnClickListener {
+            loc = location.exits["SE"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+        sw_button.setOnClickListener {
+            loc = location.exits["SW"]!!
+            location = locations[loc]!!
+            buttonUpdate(location)
+        }
+
+    }
+
+    fun buttonUpdate(location: Location) {
         description.text = location.description
         disableAllButtons()
-
         location.exits.keys.forEach {
             when(it) {
                 "U" -> up_button.isEnabled = true
@@ -53,230 +119,5 @@ class GameActivity : AppCompatActivity() {
                 "SW" -> sw_button.isEnabled = true
             }
         }
-
-        if (location.locationID == 0) {
-            disableAllButtons()
-        }
-
-        up_button.setOnClickListener {
-            Toast.makeText(this, "up", Toast.LENGTH_SHORT).show()
-            loc = location.exits["U"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        down_button.setOnClickListener {
-            Toast.makeText(this, "down", Toast.LENGTH_SHORT).show()
-            loc = location.exits["D"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        north_button.setOnClickListener {
-            Toast.makeText(this, "north", Toast.LENGTH_SHORT).show()
-            loc = location.exits["N"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        south_button.setOnClickListener {
-            Toast.makeText(this, "south", Toast.LENGTH_SHORT).show()
-            loc = location.exits["S"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        east_button.setOnClickListener {
-            Toast.makeText(this, "east", Toast.LENGTH_SHORT).show()
-            loc = location.exits["E"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        west_button.setOnClickListener {
-            Toast.makeText(this, "west", Toast.LENGTH_SHORT).show()
-            loc = location.exits["W"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        ne_button.setOnClickListener {
-            Toast.makeText(this, "north-east", Toast.LENGTH_SHORT).show()
-            loc = location.exits["NE"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        nw_button.setOnClickListener {
-            Toast.makeText(this, "north-west", Toast.LENGTH_SHORT).show()
-            loc = location.exits["NW"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        se_button.setOnClickListener {
-            Toast.makeText(this, "south-east", Toast.LENGTH_SHORT).show()
-            loc = location.exits["SE"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
-        sw_button.setOnClickListener {
-            Toast.makeText(this, "south-west", Toast.LENGTH_SHORT).show()
-            loc = location.exits["SW"]!!
-            location = locations[loc]!!
-            description.text = location.description
-            disableAllButtons()
-            location.exits.keys.forEach {
-                when(it) {
-                    "U" -> up_button.isEnabled = true
-                    "D" -> down_button.isEnabled = true
-                    "N" -> north_button.isEnabled = true
-                    "S" -> south_button.isEnabled = true
-                    "E" -> east_button.isEnabled = true
-                    "W" -> west_button.isEnabled = true
-                    "NE" -> ne_button.isEnabled = true
-                    "NW" -> nw_button.isEnabled = true
-                    "SE" -> se_button.isEnabled = true
-                    "SW" -> sw_button.isEnabled = true
-                }
-            }
-        }
-
     }
 }
--- app/src/main/java/com/odys/kotlintestapp/MainActivity.kt
+++ app/src/main/java/com/odys/kotlintestapp/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.odys.kotlintestapp
 
-import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         start_button.setOnClickListener {
-            Toast.makeText(this, "start button clicked", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, GameActivity::class.java)
             startActivity(intent)
             finish()
         }
 
         exit_button.setOnClickListener {
-            Toast.makeText(this, "exit button clicked", Toast.LENGTH_SHORT).show()
             finish()
         }
     }
